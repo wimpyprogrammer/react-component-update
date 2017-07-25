731a206da1f2f19ff433ebfa18b27c263cacab9c
Drew Keller <drew@wimpyprogrammer.com>
2017-07-24 21:49:23 -0500

    Fix redundant dependencies in install script.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -4,9 +4,11 @@ echo "installing React $REACT_VERSION"
 
 npm install
 
+npm install react@$REACT_VERSION react-dom@$REACT_VERSION
+
 # Conditionally install dependencies per https://github.com/airbnb/enzyme#installation
 if [ ${REACT_VERSION} = "0" ]; then
-	npm install react@$REACT_VERSION react-dom@$REACT_VERSION react-addons-test-utils
+	npm install react-addons-test-utils
 else
-	npm install react@$REACT_VERSION react-dom@$REACT_VERSION react-test-renderer
+	npm install react-test-renderer
 fi
