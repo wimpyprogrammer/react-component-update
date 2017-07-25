fdcf9d02d730c9b8709ec60e30d30b35ba8da26f
Drew Keller <drew@wimpyprogrammer.com>
2017-07-24 21:54:48 -0500

    Purge default dependencies during Travis build.
    Use rimraf to delete some of the dependencies from initial install.
    These may be excluded or reinstalled depending on the targeted React
    version.
    
    Technique from the Enzyme build process.
    See https://github.com/airbnb/enzyme/blob/master/package.json.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -4,6 +4,9 @@ echo "installing React $REACT_VERSION"
 
 npm install
 
+rimraf node_modules/.bin/npm node_modules/.bin/npm.cmd node_modules/react node_modules/react-dom node_modules/react-addons-test-utils node_modules/react-test-renderer
+npm prune
+
 npm install --no-save react@$REACT_VERSION react-dom@$REACT_VERSION
 
 # Conditionally install dependencies per https://github.com/airbnb/enzyme#installation
