660db654e0c376756b017cf923e6506548836514
Drew Keller <drew@wimpyprogrammer.com>
2017-07-24 21:51:50 -0500

    Do not save temp dependencies for Travis build.
    This approach is copied from the build process for Enzyme.
    See https://github.com/airbnb/enzyme/blob/master/package.json.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -4,11 +4,11 @@ echo "installing React $REACT_VERSION"
 
 npm install
 
-npm install react@$REACT_VERSION react-dom@$REACT_VERSION
+npm install --no-save react@$REACT_VERSION react-dom@$REACT_VERSION
 
 # Conditionally install dependencies per https://github.com/airbnb/enzyme#installation
 if [ ${REACT_VERSION} = "0" ]; then
-	npm install react-addons-test-utils
+	npm install --no-save react-addons-test-utils
 else
-	npm install react-test-renderer
+	npm install --no-save react-test-renderer
 fi
