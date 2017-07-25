5e42270db2de851d56ce319850a2ce92ac7f76d9
Drew Keller <drew@wimpyprogrammer.com>
2017-07-24 22:40:00 -0500

    Target appropriate version of React tester.
    When testing an older version of React, the React test helper must
    be compatible with it.
    react-addons-test-utils and react-test-rendered are versioned in
    step with react and react-dom, so we can apply the same semver
    target for all installations.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -12,9 +12,9 @@ npm install --no-save react@$REACT_VERSION react-dom@$REACT_VERSION
 
 # Conditionally install dependencies per https://github.com/airbnb/enzyme#installation
 if [ ${REACT_VERSION} = "0" ]; then
-	npm install --no-save react-addons-test-utils
+	npm install --no-save react-addons-test-utils@$REACT_VERSION
 else
-	npm install --no-save react-test-renderer
+	npm install --no-save react-test-renderer@$REACT_VERSION
 fi
 
 npm ls --depth=0
